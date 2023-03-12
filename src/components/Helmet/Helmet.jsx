eb4a2c3f3dc47b4121e0f0ba766b5dc5cee8d604
wylfreid <98716198+wylfreid@users.noreply.github.com>
2023-03-12 20:05:50 +0100

    update

--- src/components/Helmet/Helmet.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const Helmet = (props) => {
-  document.title = "Apple Discount -" + props.title;
-  return <div className="w-100"> {props.children} </div>;
-};
-
-export default Helmet;
