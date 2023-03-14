b8023e3138cc3961d1e9bf66ef198b099f72f9f9
wylfreid <98716198+wylfreid@users.noreply.github.com>
2023-03-14 18:34:11 +0100

    update

--- src/components/UI/HomeCarousel.jsx
+++ src/components/UI/HomeCarousel.jsx
@@ -95,10 +95,39 @@ const HomeCarousel = () => {
                 <Col lg="6" md="6">
                   <div className="hero__content">
                     <p className="hero__subtitle"> Trending products in {year} </p>
-                    <h2>Apple products at the best prices</h2>
+                    <h2>You can track the status of your order</h2>
                     <p className="hero__desc">
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Voluptatem, magni.
+                    We update your order status in real time.
+                    </p>
+
+                    <motion.button whileTap={{ scale: 1.2 }} className="buy__btn mt-3">
+                      <Link to="/shop">SHOP NOW</Link>
+                    </motion.button>
+                  </div>
+               
+              </Col>
+          
+          </Row>
+        </Container>
+        <img
+              className='d-block w-100 '
+                
+                src={
+                heroImg1
+                }
+            />
+        </div>
+
+        <div className="slide__content"> 
+        <Container>
+          <Row>
+              
+                <Col lg="6" md="6">
+                  <div className="hero__content">
+                    <p className="hero__subtitle"> Trending products in {year} </p>
+                    <h2>Delivery anywhere in Cameroon</h2>
+                    <p className="hero__desc">
+                    Our deliverers will contact you to fix the place of delivery.
                     </p>
 
                     <motion.button whileTap={{ scale: 1.2 }} className="buy__btn mt-3">
